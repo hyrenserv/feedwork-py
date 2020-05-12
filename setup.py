3bc4d97f5eacf982108dfcd4e3e1f61bbdf00c95
miweicong <miweicong@beyondsoft.com>
2020-05-12 14:46:15 +0800

    Update

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='python project common function.',
     author_email='miweicong@163.com',
     license='MIT Licence',
-    packages=find_packages(exclude=["unittest", "test.*"])
+    packages=find_packages(exclude=["test_suite", "test.*"])
 )
