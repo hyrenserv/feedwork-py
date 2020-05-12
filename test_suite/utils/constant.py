18c13e8d822a83fae3173d2d53fa6411f3c44967
miweicong <miweicong@beyondsoft.com>
2020-05-12 14:17:31 +0800

    Update

--- test_suite/utils/constant.py
+++ test_suite/utils/constant.py
@@ -4,7 +4,7 @@ import sys
 import feedwork.utils.constant as const
 
 # 命令行下进行整天测试时，不希望显示print的内容，执行方式为：
-# UNIT_TEST
+# PYTHONPATH=$PWD UNIT_TEST=false python3 test/utils/constant.py
 UNIT_TEST = os.getenv("UNIT_TEST", "true")
 
 # ==== 单元测试代码开始 ====
