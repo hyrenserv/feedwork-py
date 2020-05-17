4b73f0a2d324f5fe001efa257892d1bee18c089d
miweicong <miweicong@beyondsoft.com>
2020-05-17 15:04:03 +0800

    update setup

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description='python project common function.',
     author_email='miweicong@163.com',
     license='MIT Licence',
+    install_requires=['loguru'],
     packages=find_packages(exclude=["test_suite", "test.*"])
 )
