93e8755b6a9a56480cb665c5fbc507c0caf1d18f
miweicong <miweicong@beyondsoft.com>
2020-06-01 16:39:43 +0800

    Update 'setup.py'
    增加 pyyaml

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='python project common function.',
     author_email='miweicong@163.com',
     license='MIT Licence',
-    install_requires=['loguru'],
+    install_requires=['loguru', 'pyyaml'],
     packages=find_packages(exclude=["test_suite", "test.*"])
 )
